a926eb1af7139db55382c62932c9eb58c20d0762
Ushuni <nigomi.34@gmail.com>
2022-05-06 01:00:26 -0400

    de modifico el indel de teams, fields

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,19 @@ const express = require('express')
 const swaggerUI= require('swagger-ui-express')
 
 const players = require('./players')
-const teachers = require('./teachers')
-const courses = require('./courses')
+const teams = require('./teams')
+const fields = require('./fields')
 
-function routerApi(app) {
+function routerApi(app) {  
     const router= express.Router()
-    app.use('/univalle/v1', router)
+    app.use('/proyecto/v1', router)
 
     const swaggerDoc=require('../swagger.json')
     app.use('/api-docs',swaggerUI.serve, swaggerUI.setup(swaggerDoc))
 
     router.use('/players', players)
-    router.use('/teachers', teachers)
-    router.use('/courses', courses)
+    router.use('/teams', teams)
+    router.use('/fields', fields)
 }
 
 module.exports = routerApi
